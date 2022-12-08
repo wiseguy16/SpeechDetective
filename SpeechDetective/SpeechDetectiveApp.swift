9ddee672854b935de0e3f093162303819e700223
wiseguy16 <gergusa04@gmail.com>
2022-12-08 10:15:09 -0500

    Initial Commit

---
+++ SpeechDetective/SpeechDetectiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechDetectiveApp.swift
+//  SpeechDetective
+//
+//  Created by Gregory Weiss on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SpeechDetectiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
